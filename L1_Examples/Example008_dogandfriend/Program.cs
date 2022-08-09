156b0a17e40adae6568cfc1bfa4b260a05a1b0de
Zhbankova_Natalia <Natalek10.07@gmail.com>
2022-08-09 19:20:22 +0500

    new folder

---
+++ L1_Examples/Example008_dogandfriend/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите скорость первого друга: ");
+int first_friend_speed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость второго друга: ");
+int second_friend_speed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость собаки: ");
+int dog_speed = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите расстояние: ");
+int distance = Convert.ToInt32(Console.ReadLine());
+int friend = 2;
+int count = 0;
+int time = 0;
+
+while (distance > 10)
+{
+	if (friend == 1)
+	{
+		time = distance / (first_friend_speed + dog_speed);
+		friend = 2;
+	}
+	else
+	{
+		time = distance / (first_friend_speed + dog_speed);
+		friend = 1;
+	}
+distance = distance - (first_friend_speed + second_friend_speed) * time;
+count++;
+}
+Console.Write($"Собака пробежит {count} раз");
---
+++ L2_Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 14, 47, 87, 96, 4, 74, 12, 74, 6 };
+
+int n = array.Length;
+int find = 74;
+int index = 0;
+while (index < n)
+{
+	if (array[index] == find) {
+		Console.WriteLine(index);
+		break;
+	}
+	index++;
+}
